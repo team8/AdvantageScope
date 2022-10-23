8bf5ddb76223ef010c8bd5b91238cade043d4762
andrew <andrewkongfan@gmail.com>
2022-10-23 09:11:47 -0700

    we can read from txt now

--- www/modules/tabControllers/searchController.mjs
+++ www/modules/tabControllers/searchController.mjs
@@ -1,10 +1,12 @@
-import { TableController } from "./tableController.mjs";
-
 export class testTab{
     #content = null;
     #table = null;
     #tableContainer = null;
     #filterForm = null;
+    #logPath = null;
+    #logFile = new XMLHttpRequest();
+    #logRaw = "";
+    #date = new Date();
 
     timestamps = [];
     data = [];
@@ -15,15 +17,17 @@ export class testTab{
         this.#tableContainer = content.getElementsByClassName("test-table-container")[0];
         this.#table = this.#tableContainer.getElementsByClassName("test-table")[0];
         this.#filterForm = this.#content.getElementsByClassName("filters-container")[0];
+        this.#logPath = "text.txt";
         console.log(this.#filterForm.children);
         console.log((this.#table.nodeName));
 
         this.resetTable();
 
+        /*
         for (var i = 0; i < 10; i++){
             this.timestamps.push(i);
             this.data.push((i/2).toString());
-        }
+        }*/
 
         this.displayTable({lookfor: null});
 
@@ -63,6 +67,44 @@ export class testTab{
                 //console.log("add");
                 this.addRow(this.timestamps[i], this.data[i]);  
             }
+        }
+    }
+
+    async periodic() {
+        /*
+        console.log("rehehehe");
+        var logfile = new XMLHttpRequest();
+        await logfile.open("GET", "text.txt", false);
+        console.log(logfile.readyState);
+        //logfile.onreadystatechange = function () {
+            console.log("nyehhhhh");
+            //if (logfile.readyState == 4) {
+                //if (logfile.status == 200 || rawFile.status == 0) {
+                    var tmpRawText = logfile.responseText;
+                    if (tmpRawText.length > this.#logRaw) {
+                        var oldLength = this.#logRaw.length;
+                        this.#logRaw = tmpRawText;
+                        var addLogs = this.#logRaw.slice(oldLength, -1);
+                        addLogs = addLogs.split("\n");
+                        for (var i = 0; i < addLogs.length; i++){
+                            this.timestamps.push(this.#date.getMilliseconds());
+                            this.data.push(addLogs[i]);
+                        }
+                        
+                    }
+             //   }
+            //}
+        //}
+        */
+        var res = await fetch("./modules/tabControllers/text.txt", {});
+        //console.log(await res.text());
+        var arr = (await res.text()).split("\n");
+        if (arr.length > this.data.length) {
+            for (var i = this.data.length; i < arr.length; i++) {
+                this.timestamps.push(1);
+                this.data.push(arr[i]);
+            }
+            this.displayTable({ lookfor: null });
         }
     }
 }
