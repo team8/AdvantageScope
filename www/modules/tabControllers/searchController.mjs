32e773f9ab155865a261212ea8b4712a9377af2b
andrew <andrewkongfan@gmail.com>
2022-10-24 09:00:41 -0700

    Revert "Revert "table looks better""
    This reverts commit d4af0484deede693ab90f98845ffd0711baeeebc.

--- www/modules/tabControllers/searchController.mjs
+++ www/modules/tabControllers/searchController.mjs
@@ -7,6 +7,7 @@ export class testTab{
     #logFile = new XMLHttpRequest();
     #logRaw = "";
     #date = new Date();
+    #cur_lookfor = null;
 
     timestamps = [];
     data = [];
@@ -48,7 +49,9 @@ export class testTab{
     resetTable() {
         this.#table.innerHTML = "";
         var row = this.#table.insertRow(0);
-        row.insertCell(0).innerHTML = "timestamp";
+        var timestamp = row.insertCell(0);
+        timestamp.innerHTML = "timestamp";
+        timestamp.style.width = "25%";
         row.insertCell(1).innerHTML = "data";
     }
 
